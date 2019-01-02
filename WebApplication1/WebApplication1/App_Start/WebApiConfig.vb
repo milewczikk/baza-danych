b985f9253f6c0c651ac7d6ffe116340adac066cb
Karol Milewczik <kmphj1996@wp.pl>
2019-01-02 19:11:21 +0100

    Założenie aplikacji webowej asp.net chwilowo bez łączenia z bazą danych

---
+++ WebApplication1/WebApplication1/App_Start/WebApiConfig.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web.Http
+
+Public Module WebApiConfig
+    Public Sub Register(ByVal config As HttpConfiguration)
+        ' Konfiguracja i usługi składnika Web API
+
+        ' Trasy składnika Web API
+        config.MapHttpAttributeRoutes()
+
+        config.Routes.MapHttpRoute(
+            name:="DefaultApi",
+            routeTemplate:="api/{controller}/{id}",
+            defaults:=New With {.id = RouteParameter.Optional}
+        )
+    End Sub
+End Module
